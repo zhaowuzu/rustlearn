d49a6059ca6eabd4c23ed0ac8e25af2ef143d30e
zhaowuzu <yitongwuzu@qq.com>
2022-11-16 23:14:10 +0800

    写了点代码

--- x_generics_trait/src/main.rs
+++ x_generics_trait/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::PartialOrd;
+
 fn main(){
     // 原始版：在数字列表中找最大值
     let mnumber_list = vec![34,50,25,100,65];
@@ -16,6 +18,12 @@ fn main(){
     let largest2 = largest(&mnumber_list);
     println!("The largest number is {}",largest2);
 
+    // 函数抽象泛型版
+    let largest3 = largest_generics(&mnumber_list);
+    println!("The largest number is {}",largest3);
+    let largest4 = largest_generics(&vec![2.1,3.5,9.9,4.6]);
+    println!("The largest number is {}",largest4);
+
 }
 
 fn largest(list:&[i32]) -> i32 {
@@ -23,6 +31,17 @@ fn largest(list:&[i32]) -> i32 {
     for &mumber in list.iter() {
         if mumber > largest {
             largest = mumber
+        }
+    }
+    largest
+}
+
+// 泛型版
+fn largest_generics<T>(list:&[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item
         }
     }
     largest
