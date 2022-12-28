748bd31fa9427737bbe38a782b8702e12b9f2d7c
zhaowuzu <yitongwuzu@qq.com>
2022-12-28 10:41:45 +0800

    20章 基本实现线程池获取请求

---
+++ web_hello/src/bin/main.rs
@@ -0,0 +1,53 @@
+use std::{fs, thread};
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::thread::sleep;
+use std::time::Duration;
+use web_hello::ThreadPool;
+
+fn main() {
+   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+
+        // // 无脑开线程的方式，可能会被攻击
+        // thread::spawn(||{
+        //     handle_connection(stream);
+        // });
+
+        // 线程池
+        pool.execute(||{
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream:TcpStream){ //TcpStream的内部可能会改变，所以我们需要将它标记为mut.（一般情况确实不需要，这个地方是个例外）
+    let mut  buffer = [0;512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep1 = b"GET /sleep HTTP/1.1\r\n";
+    let (status_line,filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n",r"D:\zt\rust\hell\hello\web_hello\hello.html")
+    }else if buffer.starts_with(sleep1){
+        sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n",r"D:\zt\rust\hell\hello\web_hello\hello.html")
+    }else{
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n",r"D:\zt\rust\hell\hello\web_hello\404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}",status_line,contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    // 果然，这个地方还是要加点延时的，不然，文件可能压根传不出去。
+    let three_seconds = Duration::from_millis(50); // 延时100ms
+    sleep(three_seconds);
+    println!("结束一次请求");
+}
---
+++ web_hello/src/lib.rs
@@ -0,0 +1,77 @@
+use std::sync::{Arc, mpsc, Mutex};
+use std::thread;
+
+pub struct ThreadPool{
+    workers:Vec<Worker>,
+    sender:mpsc::Sender<Job>, // 发生端
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>; // Job 存放一个trait，而且还做了相关的限定
+
+impl ThreadPool{
+    /// 创建线程池
+    ///
+    /// 线程池中线程的数量
+    ///
+    /// # Panics
+    ///
+    /// 'new' 函数会在size为0时触发panic.
+    pub fn new(size:usize)->ThreadPool {
+        assert!(size>0);
+
+        let (sender,receiver) = mpsc::channel(); // 1个发送者，N个消费者
+
+        let receiver = Arc::new(Mutex::new(receiver)); // 所以使用Arc来共享资源
+
+        let mut  workers = Vec::with_capacity(size);// with_capacity会为动态数组预分配出指定空间
+
+        for id in 0..size {
+            workers.push(Worker::new(id,Arc::clone(&receiver)));
+        }
+
+        ThreadPool{
+            workers,
+            sender
+        }
+
+
+    }
+
+    pub fn execute<F>(&self,f:F)
+    where
+        F:FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap()
+    }
+}
+
+struct Worker{
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id :usize,receiver:Arc::<Mutex<mpsc::Receiver<Job>>>)->Worker {
+        // TODO thread::spawn 创建后会直接执行？？
+        let thread = thread::spawn(move ||{ // move 将引用的变量的所有权转移至闭包内，通常用于使闭包的生命周期大于所捕获的变量的原生命周期（例如将闭包返回或移至其他线程）
+            loop{
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {} got a job; executing.",id);
+                job();
+            }
+            // 一种错误的用法
+            // 原因nutex没有存在unlock方法，依赖MutexGuard<T>的生命周期。
+            // while表达式内的值会把整个代码视作自己的作用域，我们在调用job()的过程中仍然持有着receiver的锁
+            while let Ok(job) = receiver.lock().unwrap().recv(){
+                println!("Worker {} got a job; executing.",id);
+                job();
+            }
+        });
+
+        Worker{
+            id,
+            thread,
+        }
+    }
+}
